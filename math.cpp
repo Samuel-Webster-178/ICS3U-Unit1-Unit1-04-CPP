8c64535063ed0e4d52abac29f028b36f6779e767
Samuel-Webster-178 <samuel.webster@mths.ca>
2022-02-24 19:32:58 +0000

    initial commit

---
+++ math.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2022 Samuel Webster All rights reserved
+//
+// Created by Samuel Webster
+// Created on February 2022
+// This is a "Hello, World!" program with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "If a rectangle has the dimensions: " << std::endl;
+    std::cout << "5cm x 3cm" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Area is " << (5 * 3) << "cm²." << std::endl;
+    std::cout << "Perimeter is " << (2 * (5 + 3 )) << "cm." << std::endl;
+    std::cout << std::endl;
+    std::cout << "Done." << std::endl;
+}
