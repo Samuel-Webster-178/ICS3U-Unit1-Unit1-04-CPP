48767f5cca72069a2281838604f091d1a65633f7
Samuel-Webster-178 <samuel.webster@mths.ca>
2022-02-24 19:38:30 +0000

    commit msg

--- math.cpp
+++ math.cpp
@@ -7,11 +7,9 @@
 #include <iostream>
 
 int main() {
-    std::cout << "If a rectangle has the dimensions: " << std::endl;
-    std::cout << "5cm x 3cm" << std::endl;
-    std::cout << std::endl;
+    std::cout << "If a rectangle has the dimensions: 5cm x 3cm" << std::endl;
     std::cout << "Area is " << (5 * 3) << "cm²." << std::endl;
-    std::cout << "Perimeter is " << (2 * (5 + 3 )) << "cm." << std::endl;
+    std::cout << "Perimeter is " << (2 * (5 + 3)) << "cm." << std::endl;
     std::cout << std::endl;
     std::cout << "Done." << std::endl;
 }
