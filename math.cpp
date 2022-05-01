2b1f8de894794eded76d0401bb3253bb39093621
Ubuntu <ubuntu@ip-172-31-84-56.ec2.internal>
2022-05-01 20:59:45 +0000

    fix

--- math.cpp
+++ math.cpp
@@ -2,14 +2,15 @@
 //
 // Created by Samuel Webster
 // Created on February 2022
-// This is a "Hello, World!" program with proper style
+// Calculates the perimeter of a rectangle with dimmensions of 5c x 3cm
 
 #include <iostream>
 
 int main() {
+    // I Calculate the perimeter of a rectangle with dimmensions of 5c x 3cm
     std::cout << "If a rectangle has the dimensions: 5cm x 3cm" << std::endl;
     std::cout << "Area is " << (5 * 3) << "cm²." << std::endl;
     std::cout << "Perimeter is " << (2 * (5 + 3)) << "cm." << std::endl;
-    std::cout << std::endl;
+    std::cout << "" << std::endl;
     std::cout << "Done." << std::endl;
 }
