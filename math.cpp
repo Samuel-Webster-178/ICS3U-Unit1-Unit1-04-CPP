255a20638849eae550a9422c5e33aae6f2455eca
Ubuntu <ubuntu@ip-172-31-84-56.ec2.internal>
2022-06-22 17:53:49 +0000

    init

--- math.cpp
+++ math.cpp
@@ -10,8 +10,8 @@ int main() {
     // I Calculate the perimeter and area of a rectangle with dimensions of 5c x 3cm
     std::cout << "If a rectangle has the dimensions: 5cm x 3cm" << std::endl;
     std::cout << "" << std::endl;
-    std::cout << "Area is " << (5 * 3) << "cm²." << std::endl;
-    std::cout << "Perimeter is " << (2 * (5 + 3)) << "cm." << std::endl;
+    std::cout << "Area is " << (5 * 3) << " cm²." << std::endl;
+    std::cout << "Perimeter is " << (2 * (5 + 3)) << " cm." << std::endl;
     std::cout << "" << std::endl;
     std::cout << "Done." << std::endl;
 }
